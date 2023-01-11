7b7d778bed9e0b9361d8195b7eebc1581f4f301b
Fernando dos Santos Muethea <fmuethea4@gmail.com>
2023-01-11 11:10:35 +0200

    Up

--- Login.php
+++ Login.php
@@ -24,21 +24,21 @@ session_start();
     $_SESSION['password'] =$row ['password'];
     $_SESSION['nivel'] =$row ['nivel'];
 
-
-
-
     header('refresh:1;dashboard.php');
   }
 
-  else if($row['email'] == $email and $row['password'] == $password and $row['nivel']=="User")
-  $_SESSION['id'] =$row ['id'];
-  $_SESSION['nome'] =$row ['id'];
-  $_SESSION['email'] =$row ['email'];
-  $_SESSION['password'] =$row ['password'];
-  $_SESSION['nivel'] =$row ['nivel'];
+  else if($row['email'] == $email and $row['password'] == $password and $row['nivel']=="User"){
+    $_SESSION['id'] =$row ['id'];
+    $_SESSION['nome'] =$row ['id'];
+    $_SESSION['email'] =$row ['email'];
+    $_SESSION['password'] =$row ['password'];
+    $_SESSION['nivel'] =$row ['nivel'];
+  
+  
+    header('refresh:1;user.php');
+  
 
-
-  header('refresh:1;user.php');
+  }
 
   }
 
@@ -53,27 +53,33 @@ session_start();
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;500;600;700&display=swap" rel="stylesheet">
   <title>Academy</title>
 </head>
 
 <body>
 
-  <div class="login">
+  <div class="back">
+    <div class="login">
 
 
-    <form method="post" action="" class="form">
-      <h1>Gerenciamento de Estudantes</h1>
-      <div class="form-email-pass">
-        <input type="email" name="email" placeholder="Digite o email" />
+      <form method="post" action="" class="form">
+        <h1>Gerenciamento de <span>Estudantes</span> </h1>
+        <div class="form-email-pass">
+          <input type="email" name="email" placeholder="Digite o email" />
 
-        <input type="password" name="password" placeholder="Password">
-      </div>
+          <input type="password" name="password" placeholder="Password">
+        </div>
 
 
-      <input class="btn" type="submit" value="Entrar" name="btn_entrar">
-    </form>
+        <input class="btn" type="submit" value="Entrar" name="btn_entrar">
+      </form>
 
+    </div>
   </div>
+
 </body>
 
 </html>
--- dashboard.php
+++ dashboard.php
@@ -21,13 +21,51 @@ session_start();
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
   <title>Dashboard</title>
 </head>
 
 <body>
 
+  <nav>
+    <ul>
+      <a href="dashboard.php">
 
-  <h1>Bem vindo ao Dashboard</h1>
+        <?php
+
+        $select = $pdo->prepare("select * from tbl_user where nivel='Admin'");
+
+        $select->execute();
+
+        while ($row = $select->fetch(PDO::FETCH_OBJ)) {
+          
+          echo "<h1>$row->nome</h1>";
+        }
+
+
+
+      ?>
+
+      </a>
+
+      <div class="menu">
+        <li><a href="dashboard.php">Home</a></li>
+        <li><a href="/about">cadastrar estudante</a></li>
+        <li><a href="/login">Pesquisar estudante</a></li>
+        <li><a href="Login.php">Sair</a></li>
+      </div>
+
+
+    </ul>
+  </nav>
+
+  <div class="center">
+    <h1>GERENCIAMENTO DE ESTUDANTES</h1>
+    <div class="btns">
+      <button>Cadastrar estudante</button>
+      <button>Pesquisar estudante</button>
+    </div>
+  </div>
 
 </body>
 
